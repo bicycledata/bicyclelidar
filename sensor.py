8df276df22ba603037740705fa391f835cab5590
Lennart Ochel <lennart.ochel@outlook.com>
2024-10-21 19:50:09 +0200

    Add human readable timestamp to data file

--- sensor.py
+++ sensor.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 
 import argparse
-import random
+import logging
 import socket
 import threading
 import time
 from collections import deque
-import logging
+from datetime import datetime
+
 import serial
+
 from BicycleSensor import BicycleSensor, configure
 
 HOST = '0.0.0.0'
@@ -44,11 +46,13 @@ def start_server():
 
 class BicycleLidar(BicycleSensor):
   def write_header(self):
-    self.write_to_file('time, distance [cm], strength, temp')
+    self.write_to_file('time, unix_timestamp, distance [cm], strength, temp')
 
   def write_measurement(self):
     '''Write the measurement to file and broadcast to clients.'''
-    self.write_to_file(f'{str(time.time())}, {self._distance}, {self._strength}, {self._temp}')
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    unix_timestamp = time.time()  # Unix timestamp
+    self.write_to_file(f'{current_time}, {unix_timestamp}, {self._distance}, {self._strength}, {self._temp}')
     broadcast(f'lidar1:{self._distance:.1f}#'.encode('utf-8'))
 
   def worker_main(self):
